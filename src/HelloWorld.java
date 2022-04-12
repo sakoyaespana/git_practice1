771c0d5fc389078ea535b9ed84ba284aef59bf9a
saadet <sakoyaespana@gmail.com>
2022-04-12 14:13:56 +0200

    work 3 is done and about to be pushed to Githup after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,15 @@
 public class HelloWorld {
 
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+
+    }
     // Merhaba arkadaslar!
     // Hola amigas/os!
     // Hello friends!
     // Now I am adding some more data
+    // Today is very good day bu I can not because 5 hours long video
+
 }
